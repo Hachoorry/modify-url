055819818ee8dc9ce79def271c99da83491abbfc
Hachoorry <99401872+Hachoorry@users.noreply.github.com>
2023-03-06 10:43:57 +0800

    Update modify-url.py
    make it loop

--- modify-url.py
+++ modify-url.py
@@ -1,3 +1,5 @@
+#無法處裡中文！
+
 import subprocess
 import re
 
@@ -5,8 +7,12 @@ def copy2clip(txt):
     txt = re.sub(r'\W','-',txt).strip('-') # 所有非英文跟數字的都變成'-'
     txt = re.sub(r'-+','-',txt).lower()    # 把重複的'-'整理乾淨
     cmd='echo '+txt+'|clip'
-    print(f"轉換完畢，以貼入剪貼簿：\n\n{txt}\n\n==========")
+    print(f"轉換完畢，並已貼入剪貼簿：\t{txt}\n\n==========\n")
     return subprocess.check_call(cmd, shell=True)  # 執行cmd
-    
-txt = input("輸入要轉換的東東")
+
+  
+txt = input("輸入要轉換的東東：\t")
 copy2clip(txt)
+while True:
+    txt = input("繼續輸入下一筆：\t")
+    copy2clip(txt)
